09085d7ebe5276971c718d43336df22a4498f3d1
toshimihosokawa1111-corder <toshimi.hosokawa1111@gmail.com>
2026-06-16 13:14:20 +0900

    add footer and drawer menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,45 +1,56 @@
-
 jQuery(function ($) {
-  
-  // ページトップボタン
+
+  // 1. ページトップボタン
   var topBtn = $('.js-pagetop');
   topBtn.hide();
 
-  // ページトップボタンの表示設定
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
+    if ($(this).scrollTop() > 50) {
       topBtn.fadeIn();
     } else {
-      // 画面が指定pxより上ならボタンを非表示
       topBtn.fadeOut();
     }
   });
 
-  // ページトップボタンをクリックしたらスクロールして上に戻る
   topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
+    $('html, body').stop().animate({ scrollTop: 0 }, 300, 'swing');
     return false;
   });
 
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動。ヘッダーの高さ考慮。)
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
+  // 2. スムーススクロール
+  $(document).on('click', 'a[href*="#"]', function (e) {
+  e.preventDefault();
+  let hash = $(this).attr('href');
+  if (hash === '#') return false;
+  let target = $(hash);
+  if (!target.length) return false;
+  $('html,body').stop().animate({ scrollTop: target.offset().top - 50 }, 400, 'swing');
+  return false;
   });
 
+  // ヘッダーの背景色変更
+  $(window).on('scroll', function() {
+    let scrollTop = $(this).scrollTop();
+    let aboutTop = $('#about').offset().top - 50;
+
+    if (scrollTop >= aboutTop) {
+      $('.p-header').addClass('is-scrolled');
+    } else {
+      $('.p-header').removeClass('is-scrolled');
+    }
+  });
+
+  $(window).on('scroll', function() {
+  if ($('.js-drawer').hasClass('is-open')) {
+    $('.js-drawer').removeClass('is-open');
+    $('.js-hamburger').removeClass('is-open');
+    $('.js-hamburger').attr('aria-expanded', false);
+    $('.js-drawer').attr('aria-hidden', true);
+    $('.js-hamburger').attr('aria-label', 'メニューを開く');
+  }
 });
 
-$(function() {
-
-  // Swiper
+  // 3. Swiper
   const swiper = new Swiper('.p-mv__swiper', {
     loop: true,
     autoplay: {
@@ -51,20 +62,28 @@ $(function() {
     autoHeight: false,
   });
 
-});
+  // 4. Works Modal
+  $('.js-works-item').on('click', function() {
+    const modalSrc = $(this).data('modal-src');
+    const modalCaption = $(this).data('modal-caption');
+    $('.js-works-modal-img').attr('src', modalSrc);
+    $('.js-works-modal-caption').text(modalCaption);
+    $('.js-works-modal').addClass('is-open');
+    $('body').css('overflow', 'hidden');
+  });
 
-// Works Modal
-$('.js-works-item').on('click', function() {
-  const modalSrc = $(this).data('modal-src');
-  const modalCaption = $(this).data('modal-caption');
+  $('.js-works-modal-overlay, .js-works-modal-close').on('click', function() {
+    $('.js-works-modal').removeClass('is-open');
+    $('body').css('overflow', '');
+  });
 
-  $('.js-works-modal-img').attr('src', modalSrc);
-  $('.js-works-modal-caption').text(modalCaption);
-  $('.js-works-modal').addClass('is-open');
-  $('body').css('overflow', 'hidden');
-});
+  // 5. ハンバーガーメニュー単体の開閉
+  $('.js-hamburger').on('click', function() {
+    const isOpen = $('.js-drawer').toggleClass('is-open').hasClass('is-open');
+    $(this).toggleClass('is-open');
+    $(this).attr('aria-expanded', isOpen);
+    $('.js-drawer').attr('aria-hidden', !isOpen);
+    $(this).attr('aria-label', isOpen ? 'メニューを閉じる' : 'メニューを開く');
+  });
 
-$('.js-works-modal-overlay, .js-works-modal-close').on('click', function() {
-  $('.js-works-modal').removeClass('is-open');
-  $('body').css('overflow', '');
 });
